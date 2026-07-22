c924867c049e219747a9d15eeb632fcbad6dacfb
AmolMane30 <maneamol3001@gmail.com>
2026-07-22 14:47:08 +0530

    Initial clean architecture setup with domain entities and EF Core

---
+++ CRNTechnicalAssessment.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace CRNTechnicalAssessment.Application
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ CRNTechnicalAssessment.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRNTechnicalAssessment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRNTechnicalAssessment.Infrastructure.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+
+    public DbSet<Item> Items => Set<Item>();
+}
